f387cbc1efc3d50014d580655cb7dc4c0b59a6a3
Unique-Divine <realuniquedivine@gmail.com>
2023-04-18 07:06:13 -0500

    feat(bindings-perp): add dummy data to query.rs

--- contracts/bindings-perp/src/query.rs
+++ contracts/bindings-perp/src/query.rs
@@ -157,4 +157,87 @@ pub mod dummy {
         }
     }
 
+    pub fn position_response() -> PositionResponse {
+        let addr_str: &str = "nibi1kqg3q3v8pjd3epktg9h0azwk56j5v5r5lu5eq2";
+        PositionResponse {
+            position: Position {
+                trader_addr: Addr::unchecked(String::from(addr_str)),
+                pair: "ETH:USD".to_string(),
+                size: Decimal::zero(),
+                margin: Decimal::zero(),
+                open_notional: Decimal::zero(),
+                latest_cpf: Decimal::zero(),
+                block_number: 0u64.into(),
+            },
+            notional: Decimal::zero(),
+            upnl: Decimal::zero(),
+            margin_ratio_mark: Decimal::zero(),
+            margin_ratio_index: Decimal::zero(),
+            block_number: 0u64.into(),
+        }
+    }
+
+    pub fn positions_response() -> PositionsResponse {
+        PositionsResponse {
+            positions: HashMap::new(),
+        }
+    }
+
+    pub fn module_params_response() -> ModuleParamsResponse {
+        ModuleParamsResponse {
+            module_params: ModuleParams {
+                stopped: false,
+                fee_pool_fee_ratio: dec_420(),
+                ecosystem_fund_fee_ratio: dec_69(),
+                liquidation_fee_ratio: dec_69(),
+                partial_liquidation_ratio: Decimal::zero(),
+                funding_rate_interval: "1h".to_string(),
+                twap_lookback_window: Duration::Time(60 * 60), // 1 hour
+                whitelisted_liquidators: HashSet::from_iter(
+                    vec!["nibi123", "nibiabc"]
+                        .iter()
+                        .map(|s_ptr| s_ptr.to_string()),
+                ),
+            },
+        }
+    }
+
+    pub fn premium_fraction_response() -> PremiumFractionResponse {
+        PremiumFractionResponse {
+            pair: "ETH:USD".to_string(),
+            cpf: Decimal::zero(),
+            estimated_next_cpf: dec_69(),
+        }
+    }
+
+    pub fn metrics_response() -> MetricsResponse {
+        MetricsResponse {
+            metrics: Metrics {
+                pair: "ETH:USD".to_string(),
+                net_size: dec_420(),
+                volume_quote: Decimal::one(),
+                volume_base: dec_420(),
+                block_number: 42u64.into(),
+            },
+        }
+    }
+
+    pub fn module_accounts_response() -> ModuleAccountsResponse {
+        let name = "acc1";
+        let mut accounts_map = HashMap::new();
+        accounts_map.insert(
+            name.to_string(),
+            ModuleAccountWithBalance {
+                name: name.to_string(),
+                addr: Addr::unchecked(String::from("nibiacc1")),
+                balance: vec![Coin {
+                    denom: "foocoin".to_string(),
+                    amount: Uint128::new(420),
+                }],
+            },
+        );
+        ModuleAccountsResponse {
+            module_accounts: accounts_map,
+        }
+    }
 }
