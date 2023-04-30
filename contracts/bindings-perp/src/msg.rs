dcca34c7157bec01fe8ee1cdf746169b372b8e02
Unique-Divine <realuniquedivine@gmail.com>
2023-04-29 20:07:52 -0500

    feat(bindings-perp): add Repeg execute msg and dummy fns

--- contracts/bindings-perp/src/msg.rs
+++ contracts/bindings-perp/src/msg.rs
@@ -36,6 +36,7 @@ pub enum NibiruRoute {
 #[cw_serde]
 pub enum ExecuteMsg {
     OpenPosition {
+        sender: String,
         pair: String,
         is_long: bool,
         quote_amount: Uint128,
@@ -69,6 +70,11 @@ pub enum ExecuteMsg {
         sender: String,
         donation: Coin,
     },
+
+    PegShift {
+        pair: String,
+        peg_mult: Decimal,
+    },
 }
 
 #[cw_serde]
@@ -78,6 +84,7 @@ pub struct LiquidationArgs {
 }
 
 pub fn msg_open_position(
+    sender: String,
     pair: String,
     is_long: bool,
     quote_amount: Uint128,
@@ -87,6 +94,7 @@ pub fn msg_open_position(
     NibiruExecuteMsgWrapper {
         route: NibiruRoute::Perp,
         msg: ExecuteMsg::OpenPosition {
+            sender,
             pair,
             is_long,
             quote_amount,
@@ -160,4 +168,137 @@ pub fn msg_donate_to_insurance_fund(
         msg: ExecuteMsg::DonateToInsuranceFund { sender, donation },
     }
     .into()
+}
+
+pub fn msg_peg_shift(
+    pair: String,
+    peg_mult: Decimal,
+) -> CosmosMsg<NibiruExecuteMsgWrapper> {
+    NibiruExecuteMsgWrapper {
+        route: NibiruRoute::Perp,
+        msg: ExecuteMsg::PegShift { pair, peg_mult },
+    }
+    .into()
+}
+
+#[cfg(test)]
+pub mod dummy {
+
+    use std::{fs::File, io::Write, str::FromStr};
+
+    use crate::query::dummy::dec_420;
+
+    use super::*;
+
+    use cosmwasm_schema::cw_serde;
+    use cosmwasm_std::{Coin, Decimal, Uint128};
+
+    #[cw_serde]
+    pub struct ExampleExecuteMsgJson {
+        open_position: ExecuteMsg,
+        close_position: ExecuteMsg,
+        add_margin: ExecuteMsg,
+        remove_margin: ExecuteMsg,
+        multi_liquidate: ExecuteMsg,
+        donate_to_insurance_fund: ExecuteMsg,
+    }
+
+    #[test]
+    fn save_example_json() {
+        let example_msgs = ExampleExecuteMsgJson {
+            open_position: execute_open_position(),
+            close_position: execute_close_position(),
+            add_margin: execute_add_margin(),
+            remove_margin: execute_remove_margin(),
+            multi_liquidate: execute_multi_liquidate(),
+            donate_to_insurance_fund: execute_donate_to_insurance_fund(),
+        };
+        let json_str = serde_json::to_string_pretty(&example_msgs).unwrap();
+        let mut file = File::create("./execute_msg.json").unwrap();
+        assert!(file.write_all(json_str.as_bytes()).is_ok());
+    }
+
+    static DUMMY_ADDR: &str = "nibi1zaavvzxez0elundtn32qnk9lkm8kmcsz44g7xl";
+    static DUMMY_ADDR_2: &str = "nibi1ah8gqrtjllhc5ld4rxgl4uglvwl93ag0sh6e6v";
+    static DUMMY_PAIR: &str = "ETH:USD";
+
+    pub fn execute_open_position() -> ExecuteMsg {
+        ExecuteMsg::OpenPosition {
+            sender: DUMMY_ADDR.to_string(),
+            pair: DUMMY_PAIR.to_string(),
+            is_long: true,
+            quote_amount: Uint128::from(100u128),
+            leverage: Decimal::from_str("5").unwrap(),
+            base_amount_limit: Uint128::from(1000u128),
+        }
+    }
+
+    pub fn execute_close_position() -> ExecuteMsg {
+        ExecuteMsg::ClosePosition {
+            sender: DUMMY_ADDR.to_string(),
+            pair: DUMMY_PAIR.to_string(),
+        }
+    }
+
+    pub fn execute_add_margin() -> ExecuteMsg {
+        ExecuteMsg::AddMargin {
+            sender: DUMMY_ADDR.to_string(),
+            pair: DUMMY_PAIR.to_string(),
+            margin: Coin {
+                denom: "uusd".to_string(),
+                amount: Uint128::from(100u128),
+            },
+        }
+    }
+
+    pub fn execute_remove_margin() -> ExecuteMsg {
+        ExecuteMsg::RemoveMargin {
+            sender: DUMMY_ADDR.to_string(),
+            pair: DUMMY_PAIR.to_string(),
+            margin: Coin {
+                denom: "uusd".to_string(),
+                amount: Uint128::from(100u128),
+            },
+        }
+    }
+
+    pub fn execute_multi_liquidate() -> ExecuteMsg {
+        ExecuteMsg::MultiLiquidate {
+            pair: DUMMY_PAIR.to_string(),
+            liquidations: vec![
+                LiquidationArgs {
+                    pair: DUMMY_PAIR.to_string(),
+                    trader: DUMMY_ADDR.to_string(),
+                },
+                LiquidationArgs {
+                    pair: DUMMY_PAIR.to_string(),
+                    trader: DUMMY_ADDR_2.to_string(),
+                },
+            ],
+        }
+    }
+
+    pub fn execute_donate_to_insurance_fund() -> ExecuteMsg {
+        ExecuteMsg::DonateToInsuranceFund {
+            sender: DUMMY_ADDR.to_string(),
+            donation: Coin {
+                denom: "uusd".to_string(),
+                amount: Uint128::from(100u128),
+            },
+        }
+    }
+
+    pub fn execute_peg_shift() -> ExecuteMsg {
+        ExecuteMsg::PegShift {
+            pair: DUMMY_PAIR.to_string(),
+            peg_mult: dec_420(),
+        }
+    }
+
+    pub fn liquidation_args() -> LiquidationArgs {
+        LiquidationArgs {
+            pair: DUMMY_PAIR.to_string(),
+            trader: "trader".to_string(),
+        }
+    }
 }
