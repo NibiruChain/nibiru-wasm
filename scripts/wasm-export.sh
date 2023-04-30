cbfbf214dccefc64cdd02015668133c0ddf28daa
Unique-Divine <realuniquedivine@gmail.com>
2023-04-29 20:10:31 -0500

    build(contracts): compile contracts w new dependencies

--- scripts/wasm-export.sh
+++ scripts/wasm-export.sh
@@ -1,2 +1,3 @@
 cp artifacts/bindings_perp.wasm ../nibiru/x/wasm/binding/wasmbin/bindings_perp.wasm
-cp contracts/bindings-perp/queries.json ../nibiru/x/wasm/binding/cw_struct/queries.json
+cp contracts/bindings-perp/query_resp.json ../nibiru/x/wasm/binding/cw_struct/query_resp.json
+cp contracts/bindings-perp/execute_msg.json ../nibiru/x/wasm/binding/cw_struct/execute_msg.json
