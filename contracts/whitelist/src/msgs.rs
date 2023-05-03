b73385ecc6f4b3edc11494ed3088e277320a673e
Unique-Divine <realuniquedivine@gmail.com>
2023-05-03 13:35:54 -0500

    feat(whitelist): admin check fns, query impl, + tests

--- contracts/whitelist/src/msgs.rs
+++ contracts/whitelist/src/msgs.rs
@@ -1,31 +1,34 @@
-use std::collections::HashSet;
-
 use cosmwasm_schema::cw_serde;
 
+use crate::state::Whitelist;
+
+/// InitMsg specifies the args for the instantiate entry point of the contract.
 #[cw_serde]
 pub struct InitMsg {
     pub admin: String,
 }
 
+/// ExecuteMsg specifies the args for the execute entry point of the contract.
 #[cw_serde]
 pub enum ExecuteMsg {
     AddMember { address: String },
     RemoveMember { address: String },
 }
 
+/// QueryMsg specifies the args for the query entry point of the contract.
 #[cw_serde]
 pub enum QueryMsg {
     IsMember { address: String },
-    Members {},
+    Whitelist {},
 }
 
 #[cw_serde]
 pub struct IsMemberResponse {
     pub is_member: bool,
+    pub whitelist: Whitelist,
 }
 
 #[cw_serde]
-pub struct MembersResponse {
-    pub admin: String,
-    pub members: HashSet<String>,
+pub struct WhitelistResponse {
+    pub whitelist: Whitelist,
 }
