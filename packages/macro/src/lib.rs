406ad0097046138a9467dd0ddac0949a43121cbe
Unique-Divine <realuniquedivine@gmail.com>
2023-05-07 20:08:03 -0500

    feat(macro): implement cw_custom procedural macro for creating custom msgs

---
+++ packages/macro/src/lib.rs
@@ -0,0 +1,41 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+/// cw_custom: A procedural macro attribute that generates implementations of:
+/// (1) the `cosmwasm_std::CustomMsg` trait and (2) the `From` trait for
+/// converting the input type into an instance of `cosmwasm_std::CosmosMsg`.
+///
+/// # Example
+///
+/// ```
+/// use nibiru_macro::cw_custom;
+/// use cosmwasm_std::{CustomMsg, CosmosMsg};
+/// use cosmwasm_schema::{cw_serde};
+///
+/// #[cw_serde]
+/// #[cw_custom]
+/// pub struct MyCustomMsg {
+///   // struct fields...
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn cw_custom(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    let name = &ast.ident;
+
+    let gen = quote! {
+        #ast
+
+        impl CustomMsg for #name {}
+
+        impl From<#name> for CosmosMsg<#name> {
+            fn from(original: #name) -> Self {
+                CosmosMsg::Custom(original)
+            }
+        }
+    };
+
+    gen.into()
+}
