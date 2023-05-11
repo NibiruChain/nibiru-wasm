3b03f45f213ab2371a2635bab1312b29c9ca180b
Unique-Divine <realuniquedivine@gmail.com>
2023-05-11 17:03:19 -0500

    feat(scripts): add shifter to wasm-export.sh

--- scripts/wasm-export.sh
+++ scripts/wasm-export.sh
@@ -1,3 +1,4 @@
 cp artifacts/bindings_perp.wasm ../nibiru/x/wasm/binding/wasmbin/bindings_perp.wasm
+cp artifacts/shifter.wasm ../nibiru/x/wasm/binding/wasmbin/shifter.wasm
 cp contracts/bindings-perp/query_resp.json ../nibiru/x/wasm/binding/cw_struct/query_resp.json
 cp contracts/bindings-perp/execute_msg.json ../nibiru/x/wasm/binding/cw_struct/execute_msg.json
