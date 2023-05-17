a90faac928009d0c77ccd96631f6e88cd39297c8
Jonathan Gimeno <jgimeno@gmail.com>
2023-05-17 12:47:48 +0200

    add last oracle param update fields (#28)
    * add last fields
    
    * build: run tidy and build the optimized .wasm
    
    ---------
    
    Co-authored-by: Unique-Divine <realuniquedivine@gmail.com>

--- contracts/controller/src/msgs.rs
+++ contracts/controller/src/msgs.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Uint256, Uint64};
+use cosmwasm_std::{Decimal, Uint256, Uint64};
 
 use crate::state::Whitelist;
 
@@ -12,13 +12,36 @@ pub struct InitMsg {
 /// ExecuteMsg specifies the args for the execute entry point of the contract.
 #[cw_serde]
 pub enum ExecuteMsg {
-    SetMarketEnabled { pair: String, enabled: bool },
-    InsuranceFundWithdraw { amount: Uint256, to: String },
-    EditOracleParams { vote_period: Option<Uint64> },
+    SetMarketEnabled {
+        pair: String,
+        enabled: bool,
+    },
+    InsuranceFundWithdraw {
+        amount: Uint256,
+        to: String,
+    },
+    EditOracleParams {
+        vote_period: Option<Uint64>,
+        vote_threshold: Option<Decimal>,
+        reward_band: Option<Decimal>,
+        whitelist: Option<Vec<String>>,
+        slash_fraction: Option<Decimal>,
+        slash_window: Option<Uint64>,
+        min_valid_per_window: Option<Decimal>,
+        twap_lookback_window: Option<Uint64>,
+        min_voters: Option<Uint64>,
+        validator_fee_ratio: Option<Decimal>,
+    },
 
-    AddMember { address: String },
-    RemoveMember { address: String },
-    ChangeAdmin { address: String },
+    AddMember {
+        address: String,
+    },
+    RemoveMember {
+        address: String,
+    },
+    ChangeAdmin {
+        address: String,
+    },
 }
 
 /// QueryMsg specifies the args for the query entry point of the contract.
