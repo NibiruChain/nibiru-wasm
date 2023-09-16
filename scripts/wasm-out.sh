85719bed9b3ac6832468c2b0e38280ea35490c0a
Unique-Divine <realuniquedivine@gmail.com>
2023-09-16 17:05:50 -0500

    build: use latest cosmwasm/workspace-optimizer image + rebuild all contracts

--- scripts/wasm-out.sh
+++ scripts/wasm-out.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# 
-# wasm-out.sh 
-# 
-# Compile contracts into .wasm bytecode using rust-optimizer 
+#
+# wasm-out.sh
+#
+# Compile contracts into .wasm bytecode using rust-optimizer
 # Optimize to reduce gas
 #
 # Ref: https://github.com/CosmWasm/rust-optimizer
@@ -10,14 +10,14 @@
 # Compiles CosmWasm smart contracts to WebAssembly bytecode (.wasm)
 wasm() {
   local image="$1"
-  local image_version="0.12.12"
+  local image_version="0.14.0"
   docker run --rm -v "$(pwd)":/code \
     --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
     cosmwasm/$image:$image_version
 }
 
-# wasm - single contract 
+# wasm - single contract
 wasm_single() {
   echo "Running rust-optimizer for a single contract (cosmwasm/rust-optimizer)"
   wasm rust-optimizer
@@ -39,8 +39,8 @@ main() {
   fi
 }
 
-if main; then 
+if main; then
   echo "🔥 Compiled all smart contracts successfully. "
-else 
+else
   echo "❌ Compilation failed."
 fi
